9879c28f688508954a0510f0b798631419006c13
dmccarthy <dmccarthy@tssg.org>
2014-08-26 09:07:09 +0100

    Fixed typo

--- build-cloudlet-sdk.sh
+++ build-cloudlet-sdk.sh
@@ -7,7 +7,7 @@ scala -cp target/scala-2.10/swagger-codegen.jar -DskipErrors=true eu.openict.cod
 
 cd generated-code/android-java/
 
-cp ../../../openi-custom-code/OPENiobject.java src/main/java/eu/openiict/client/model/
+cp ../../../openi-custom-code/OPENiObject.java src/main/java/eu/openiict/client/model/
 
 sed -i 's/<httpclient-version>4.0/<httpclient-version>4.3.2/' pom.xml
 
