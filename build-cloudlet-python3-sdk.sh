081a76cfdaabf0fbd2f8b563a54a4a47e24db152
dmccarthy <dmccarthy@tssg.org>
2014-09-04 15:31:34 +0100

    Moved to https

--- build-cloudlet-python3-sdk.sh
+++ build-cloudlet-python3-sdk.sh
@@ -3,5 +3,5 @@ cd swagger-codegen
 
 rm -fr generated-code/python3
 
-scala -cp target/scala-2.10/swagger-codegen.jar -DskipErrors=true com.wordnik.swagger.codegen.BasicPython3Generator http://$1/api-spec/v1/cloudlet
+scala -cp target/scala-2.10/swagger-codegen.jar -DskipErrors=true com.wordnik.swagger.codegen.BasicPython3Generator https://$1/api-spec/v1/cloudlet
 
