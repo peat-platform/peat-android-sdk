dd25c40eabcd29ecd5030aa08be4baf5e3e43093
dmccarthy <dmccarthy@tssg.org>
2014-09-13 13:53:55 +0300

    Changed Context to Java Map

--- build-graph-api-sdk.sh
+++ build-graph-api-sdk.sh
@@ -10,9 +10,9 @@ scala -cp target/scala-2.10/swagger-codegen.jar -DskipErrors=true eu.openiict.co
 sed -i 's/import eu.openiict.client.model.Datetime;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
 sed -i 's/import eu.openiict.client.model.Integer;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
 sed -i 's/import eu.openiict.client.model.Object;//' generated-code/android-java/src/main/java/eu/openiict/client/api/*
-sed -i 's/import eu.openiict.client.model.Related;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/related/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/Related/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/import eu.openiict.client.model.Related;/import java.util.Map;/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/related/Map/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/Related/Map/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
 sed -i 's/datetime/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
 
 cd generated-code/android-java/
