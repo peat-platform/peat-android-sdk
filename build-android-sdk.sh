6927dbec8560022dbd67c8967556ec755d9c3adc
dmccarthy <dmccarthy@tssg.org>
2014-09-13 09:06:27 +0300

    Fixed graph api def

--- build-android-sdk.sh
+++ build-android-sdk.sh
@@ -5,6 +5,14 @@ rm -fr generated-code/android-java
 
 scala -cp  target/scala-2.10/swagger-codegen.jar -DskipErrors=true eu.openiict.codegen.AndroidOPENiApiCodegen http://$1/api-spec/v1
 
+sed -i 's/import eu.openiict.client.model.Datetime;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/import eu.openiict.client.model.Integer;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/import eu.openiict.client.model.Object;//' generated-code/android-java/src/main/java/eu/openiict/client/api/*
+sed -i 's/import eu.openiict.client.model.Related;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/related/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/Related/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+sed -i 's/datetime/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+
 cd generated-code/android-java/
 
 cp ../../../openi-custom-code/OPENiObject.java src/main/java/eu/openiict/client/model/
