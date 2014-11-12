0aa2538550a1d89d110b0337c214718aef3be26c
dmccarthy <dmccarthy@tssg.org>
2014-11-12 16:09:03 +0000

    Added first version of Android bundle client

--- build-cloudlet-sdk.sh
+++ build-cloudlet-sdk.sh
@@ -2,6 +2,11 @@
 cd swagger-codegen
 
 rm -fr generated-code/android-java
+rm -fr openi_android_project/openi-client-lib/libs/*
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/api
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/common
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/model
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/utils
 
 scala -cp target/scala-2.10/swagger-codegen.jar -DskipErrors=true eu.openiict.codegen.AndroidOPENiApiCodegen http://$1/api-spec/v1/cloudlet
 
@@ -11,6 +16,18 @@ cp -r ../../../openi-custom-code/src/main/java src/main/
 
 mvn package
 
+mvn dependency:copy-dependencies
+
 cp target/openi-android-client-1.0.0.jar ../../../openi-cloudlet-android-sdk-1.0.0.jar
+
+cd ~/repos/openi_android_sdk/openi_android_project
+
+cp -r ../swagger-codegen/generated-code/android-java/src/main/java/eu openi-client-lib/src/main/java
+
+cp -r ../swagger-codegen/generated-code/android-java/target/dependency/* openi-client-lib/libs
+
+bash gradlew assemble
+
+cp openi-client-lib/build/outputs/aar/openi-client-lib.aar ..
 
 
---
+++ openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/permissions/PermissionsActivity.java
@@ -0,0 +1,60 @@
+package eu.openiict.client.permissions;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class PermissionsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    // Profile
+    public static final String KEY_PROFILE_ASSIGNMENT = "pref_profile_assignment";
+
+    // Location
+    public static final String KEY_LOCATION_PROVIDER = "pref_location_provider";
+    public static final String KEY_LOCATION_ASSIGNMENT = "pref_location_assignment";
+
+    // Media
+    public static final String KEY_MEDIA_ASSIGNMENT = "pref_media_assignment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Profile
+        Preference profileAssignmentPreference = findPreference(KEY_PROFILE_ASSIGNMENT);
+        profileAssignmentPreference.setSummary(sharedPreferences.getString(KEY_PROFILE_ASSIGNMENT, "Current App"));
+
+        // Location
+        Preference locationProviderPreference = findPreference(KEY_LOCATION_PROVIDER);
+        locationProviderPreference.setSummary(sharedPreferences.getString(KEY_LOCATION_PROVIDER, ""));
+        Preference locationAssignmentPreference = findPreference(KEY_LOCATION_ASSIGNMENT);
+        locationAssignmentPreference.setSummary(sharedPreferences.getString(KEY_LOCATION_ASSIGNMENT, "Current App"));
+
+        // Media
+        Preference mediaAssignmentPreference = findPreference(KEY_MEDIA_ASSIGNMENT);
+        mediaAssignmentPreference.setSummary(sharedPreferences.getString(KEY_MEDIA_ASSIGNMENT, "Current App"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(KEY_LOCATION_PROVIDER)) {
+            Preference locationProviderPreference = findPreference(KEY_LOCATION_PROVIDER);
+            locationProviderPreference.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+}
