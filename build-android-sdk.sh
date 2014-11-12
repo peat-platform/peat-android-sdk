06869b09b9d487b77fc485a707b241ee0630b32f
dmccarthy <dmccarthy@tssg.org>
2014-11-12 16:12:45 +0000

    Added first version of Android bundle client

--- build-android-sdk.sh
+++ build-android-sdk.sh
@@ -2,21 +2,30 @@
 cd swagger-codegen
 
 rm -fr generated-code/android-java
+rm -fr openi_android_project/openi-client-lib/libs/*
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/api
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/common
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/model
+rm -fr openi_android_project/openi-client-lib/src/main/java/eu/openiict/client/utils
 
-scala -cp  target/scala-2.10/swagger-codegen.jar -DskipErrors=true eu.openiict.codegen.AndroidOPENiApiCodegen http://$1/api-spec/v1
-
-sed -i 's/import eu.openiict.client.model.Datetime;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/import eu.openiict.client.model.Integer;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/import eu.openiict.client.model.Object;//' generated-code/android-java/src/main/java/eu/openiict/client/api/*
-sed -i 's/import eu.openiict.client.model.Related;//' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/related/Object/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/Related/Object/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
-sed -i 's/datetime/String/' generated-code/android-java/src/main/java/eu/openiict/client/model/*
+scala -cp target/scala-2.10/swagger-codegen.jar -DskipErrors=true eu.openiict.codegen.AndroidOPENiApiCodegen http://$1/api-spec/v1
 
 cd generated-code/android-java/
 
-cp -r ../../../openi-custom-code/src/main/java src/main/cd
+cp -r ../../../openi-custom-code/src/main/java src/main/
 
 mvn package
 
+mvn dependency:copy-dependencies
+
 cp target/openi-android-client-1.0.0.jar ../../../openi-android-sdk-1.0.0.jar
+
+cd ~/repos/openi_android_sdk/openi_android_project
+
+cp -r ../swagger-codegen/generated-code/android-java/src/main/java/eu openi-client-lib/src/main/java
+
+cp -r ../swagger-codegen/generated-code/android-java/target/dependency/* openi-client-lib/libs
+
+bash gradlew assemble
+
+cp openi-client-lib/build/outputs/aar/openi-client-lib.aar ..
