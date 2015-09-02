62b7442263d52e8eba716605489e9f2f8f0eeb02
Dylan Conway <funi1234@gmail.com>
2015-09-02 15:25:09 +0100

    updated sbt path

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 sudo apt-get update -y
 sudo apt-get install -y sbt
 
-wget -P /home/vagrant/.sbt/launchers/0.13.0/ https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0/sbt-launch.jar
+wget -P ~/.sbt/launchers/0.13.0/ https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0/sbt-launch.jar
 
 rm -fr scala-2.10.3.deb
 
